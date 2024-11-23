c973eb6fe11a826cfc7949c9e0765c9a8f5a4649
Fábio Ferreira <ffcfpten@gmail.com>
2024-11-23 11:07:38 +0000

    fix: 🐛 lint command name

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 const eslint = require("@eslint/js");
-const tselint = require("typescript-eslint");
+const tsEslint = require("typescript-eslint");
 const eslintConfigPrettier = require("eslint-config-prettier");
 
 module.exports = [
@@ -7,7 +7,7 @@ module.exports = [
     files: ["./src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: {
       ...eslint.configs.recommended.rules,
-      ...tselint.configs.recommended,
+      ...tsEslint.configs.recommended,
       ...eslintConfigPrettier.rules,
     },
   },
